2661409eeb524c7a56e31e1efc32291d85d86d25
haojie06 <whaojie06@gmail.com>
2023-04-24 05:21:55 +0800

    update

--- internal/discordmd/discord.go
+++ internal/discordmd/discord.go
@@ -67,7 +67,7 @@ func (m *MidJourneyService) Imagine(prompt string, params string) (taskId string
 	m.rwLock.Lock()
 	defer m.rwLock.Unlock()
 
-	prompt = strconv.Itoa(rand.Intn(1000)) + strings.Trim(strings.Trim(prompt, " ")+" "+params, " ")
+	prompt = strconv.Itoa(rand.Intn(1000)) + " " + strings.Trim(strings.Trim(prompt, " ")+" "+params, " ")
 	taskId = getHashFromPrompt(prompt)
 
 	taskResultChannel = make(chan *ImageGenerationResult, 10)
